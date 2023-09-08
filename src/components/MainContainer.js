5be8261a16914ce43cc288dddb8fb7f562112531
sjkim <07nevergiveup08@gmail.com>
2023-09-08 19:40:21 +0900

    백그라운드 MainContainer 추가.

---
+++ src/components/MainContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: var(--color-background);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const MainContainer = () => {
+  return <Container>MainContainer</Container>;
+};
+
+export default MainContainer;
